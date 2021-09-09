8a132775266f06d9e00bf4a0b80997a1eb9fc19c
Gustavo Carmo <gustavo.silva.carmo@hotmail.com>
2021-09-08 21:39:46 -0300

    test ignite SOLID

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,16 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id: userId } = request.headers;
+    const user_id = userId as string;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.status(200).json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 }
 
